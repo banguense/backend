61a8224dbf979e8feaa798f5f0839c04098a179e
Hector Fernandes <hectorhu17@gmail.com>
2025-01-05 16:09:15 -0300

    add base to project

---
+++ app/src/main/java/io/github/devhector/mpi_execute_api/MpiExecuteApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.devhector.mpi_execute_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpiExecuteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpiExecuteApiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/io/github/devhector/mpi_execute_api/MpiExecuteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devhector.mpi_execute_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpiExecuteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
