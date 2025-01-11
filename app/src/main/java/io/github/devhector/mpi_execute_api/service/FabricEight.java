80423aa019e442c46b08b6cf5018f71ed986c5db
Hector Fernandes <hectorhu17@gmail.com>
2025-01-11 18:02:45 -0300

    create a job to run a c code

--- app/src/main/java/io/github/devhector/mpi_execute_api/service/FabricEight.java
+++ app/src/main/java/io/github/devhector/mpi_execute_api/service/FabricEight.java
@@ -1,5 +1,6 @@
 package io.github.devhector.mpi_execute_api.service;
 
+import io.fabric8.kubernetes.api.model.EmptyDirVolumeSource;
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.api.model.batch.v1.JobBuilder;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
@@ -27,4 +28,45 @@ public class FabricEight implements KubernetesClient {
     }
   }
 
+  @Override
+  public String run(JobRequest request) {
+    try (io.fabric8.kubernetes.client.KubernetesClient client = clientBuilder.build()) {
+      Job job = jobBuilder.withNewMetadata().withName(request.getUuid()).endMetadata()
+          .withNewSpec().withTtlSecondsAfterFinished(5)
+          .withNewTemplate()
+          .withNewMetadata().withName("mpi-job-template").endMetadata()
+          .withNewSpec().addNewContainer()
+          .withName("runner").withImage("openmpi-alpine:latest")
+          .withCommand("sh", "-c",
+              "echo '" + request.getCode() + "' > /code/main.c && gcc /code/main.c -o /code/main && /code/main")
+          .addNewVolumeMount().withName("code-volume").withMountPath("/code").endVolumeMount().endContainer()
+          .withRestartPolicy("Never")
+          .addNewVolume().withName("code-volume").withEmptyDir(new EmptyDirVolumeSource()).endVolume()
+          .endSpec().endTemplate().endSpec().build();
+
+      client.batch().v1().jobs().resource(job).create();
+
+      boolean completed = false;
+
+      while (!completed) {
+        Job currentJob = client.batch().v1().jobs().inNamespace("default").withName(request.getUuid()).get();
+
+        if (currentJob.getStatus() != null && currentJob.getStatus().getSucceeded() != null
+            && currentJob.getStatus().getSucceeded() == 1) {
+          completed = true;
+        }
+        Thread.sleep(500);
+      }
+
+      String podName = client.pods().inNamespace("default").withLabel("job-name", request.getUuid()).list().getItems()
+          .get(0).getMetadata().getName();
+
+      String logs = client.pods().inNamespace("default").withName(podName).getLog();
+
+      return logs;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "erro";
+    }
+  }
 }
